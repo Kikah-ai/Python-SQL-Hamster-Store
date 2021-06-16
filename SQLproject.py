524158025769248f21a71eccb1c5b96991cedbc7
Kikah-ai <53788217+Kikah-ai@users.noreply.github.com>
2021-06-15 23:27:04 -0400

    SQL and Python Connection Database

---
+++ SQLproject.py
@@ -0,0 +1,20 @@
+import mysql.connector
+import pandas as pd
+from mysql.connector import Error  #seperate import so access to function is easily made
+
+#establishing connection to server
+def enable_connection(name_host, name_user, password_user): #arguments function takes
+    connection = None 
+    #possible errors
+    try: 
+        connection = mysql.connector.connect(
+            host = name_host
+            user = name_user
+            password = password_user
+        )
+        print("Successful connection to MYSQL Database.")
+    except Error as unsuccessful:
+        print(f"Unsuccessful:'{unsuccessful}'")
+        
+    retun connection
+    #function returning to SQL Server
