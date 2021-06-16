19550b5cd2aa48cb12831d215872dd9ac9d1233d
Kikah-ai <53788217+Kikah-ai@users.noreply.github.com>
2021-06-16 11:57:56 -0400

    Update SQLproject.py

--- SQLproject.py
+++ SQLproject.py
@@ -1,4 +1,5 @@
 import mysql.connector
+#mainly used as data analysis and allows data manipulation
 import pandas as pd
 from mysql.connector import Error  #seperate import so access to function is easily made
 
@@ -22,3 +23,15 @@ def enable_connection(name_host, name_user, password_user): #arguments function 
     return connection
     #function returning to SQL Server
     #connection = enable_connection("localhost", "root", pw) establishes that the server is connected
+    
+    #reusable function for creating databases
+#(connection object, SQL query)
+def generate_database(connection, query):
+    #creation of cursor object since MYSQL uses object oriented programming
+    #think of as access to blinking cursor in MYSQL
+    mouse = connection.cursor()
+    try:
+        mouse.execute(query)
+        print("Database was created.")
+    except Error as unsuccessful:
+        print(f"Unsuccessful:'{unsuccessful}'")
