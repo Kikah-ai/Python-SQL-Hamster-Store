0c42d80d66cd03b3c2dd7a38d03fc249a9a89ae8
Kikah-ai <53788217+Kikah-ai@users.noreply.github.com>
2021-06-16 22:15:34 -0400

    Update Hamstersql.sql

--- Hamstersql.sql
+++ Hamstersql.sql
@@ -13,3 +13,29 @@ CREATE TABLE seller (
     age INT,
     phone VARCHAR(25)
     );
+#Drop is to delete table
+CREATE TABLE petstore (
+	petstore_id INT PRIMARY KEY,
+    store_name VARCHAR(45) NOT NULL,
+    address VARCHAR(65) NOT NULL,
+    owner_name(50) NOT NULL
+);
+CREATE TABLE customer ( 
+	customer_id INT PRIMARY KEY,
+    first_name VARCHAR(45) NOT NULL,
+    last_name VARCHAR(45) NOT NULL,
+    phone VARCHAR(25),
+    petstore int
+);
+CREATE TABLE hamster (
+	hammy_id INT PRIMARY KEY,
+    classification VARCHAR(70) NOT NULL,
+    age VARCHAR(50) NOT NULL,
+    lifespan VARCHAR(50),
+    gender VARCHAR(50),
+    size_inch INT,
+    color VARCHAR(80),
+    seller INT,
+    petstore INT
+);
+    
